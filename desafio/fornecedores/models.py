323a1b1aa351f8b54b055a7cf5b613f160b1ffa3
Arilson-X <arilsonouzacosta0@gmail.com>
2022-03-26 17:15:43 -0300

    Atualizado o arquivo fornecedores/models.py

--- desafio/fornecedores/models.py
+++ desafio/fornecedores/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
 from django.db import models
+from django.forms import CharField
 
-# Create your models here.
+class registro(models.Model):
+    id = models.CharField(max_length=16)
+    name = models.CharField(max_length= 10)
+    company = models.CharField(max_length= 30)
+    created_at = models.DateField()
+    amount_products = models.IntegerField()
+
+    def __str__(self):
+        return self.id
