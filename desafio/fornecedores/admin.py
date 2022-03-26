778bbdb84e76c634a90f4db3baf8c2607284e813
Arilson-X <arilsonouzacosta0@gmail.com>
2022-03-26 19:22:24 -0300

    Alterado o arquivoamin.py, criada a classe Registros

--- desafio/fornecedores/admin.py
+++ desafio/fornecedores/admin.py
@@ -1,3 +1,11 @@
+from re import search
 from django.contrib import admin
+from fornecedores.models import RegistroFornecedores
 
-# Register your models here.
+class Registros(admin.ModelAdmin):
+    list_display = ('id','name','company','created_at','amount_products')
+    list_display_link = ('id','name','company')
+    search_fiels = ('id','name','company',)
+    list_per_page = 16
+
+admin.site.register(RegistroFornecedores,Registros)
