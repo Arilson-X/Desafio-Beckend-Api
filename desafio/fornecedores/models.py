e4296c9357448fbecd1f47f395b28995a4b279da
Arilson-X <arilsonouzacosta0@gmail.com>
2022-03-26 19:24:04 -0300

    Atualizado o arquivo models.py, criada a classe RegistrosFornecedores

--- desafio/fornecedores/models.py
+++ desafio/fornecedores/models.py
@@ -1,9 +1,10 @@
+from http.client import LENGTH_REQUIRED
 from pyexpat import model
 from django.db import models
 from django.forms import CharField
 
-class registro(models.Model):
-    id = models.CharField(max_length=16)
+class RegistroFornecedores(models.Model):
+    id = models.CharField(max_length=16,null=False,primary_key=True)
     name = models.CharField(max_length= 10)
     company = models.CharField(max_length= 30)
     created_at = models.DateField()
