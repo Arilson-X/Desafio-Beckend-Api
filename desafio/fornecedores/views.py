24dddd09b7f0488941e7e0d63849c10d5014967b
Arilson-X <arilsonouzacosta0@gmail.com>
2022-03-26 19:25:42 -0300

    Atualizado o arquivo views.py, adicionado a classe RegistrosViewSet

--- desafio/fornecedores/views.py
+++ desafio/fornecedores/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from fornecedores.models import RegistroFornecedores
+from fornecedores.serializer import RegistroSerializer
 
-# Create your views here.
+class RegistrosViewSet(viewsets.ModelViewSet):
+    queryset = RegistroFornecedores.objects.all()
+    serializer_class = RegistroSerializer
